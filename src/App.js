894d06506b8a055ab07625461054fc4ff32f46d2
lee-sangmo <cochiny@gmail.com>
2024-11-27 20:30:37 +0900

    myapp03

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './component/Header';
+import DayList from './component/DayList';
+import Day from './component/Day';
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        {/* 라우터의 영향을 받지 않고 고정된 컴포넌트 */}
+        <Header />
+        <Routes>
+          {/* path="/"" 는 첫페이지를 의미한다(home) */}
+          <Route path="/" element={<DayList />} />
+
+          {/* 동적 라운딩 */}
+          <Route path='/day/:day' element={<Day />} />
+
+
+          <Route path='/create_word' element={<CreateWord />} />
+          <Route path='/create_day' element={<CreateDay />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/component/DayList.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+//import dummy from "../db/data.json"
+
+function DayList(props) {
+    // console.log(dummy);
+    // const [days, setDays] = useState([]);
+
+    // useEffect(() => {
+    //     fetch('http://localhost:3010/days')
+    //         .then(res => {
+    //             return res.json()
+    //         })
+    //         .then(data => {
+    //             console.log(data);
+    //             setDays(data);
+    //         })
+    //         .catch((error) => console.error("Error : ", error));
+    // }, []);
+
+    const days = useFetch('http://localhost:3010/days');
+
+    return (
+        <ul className="list_day">
+            {days.map(k => {
+                return <li key={k.id}>
+                    <Link to={`/day/${k.day}`}> Day {k.day}</Link>
+                </li>
+            })}
+        </ul>
+    );
+}
+
+export default DayList;
